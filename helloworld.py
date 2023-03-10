4fa8e21bdbcdd6eb14a1a89e663caffc2a524f16
looka <channel.tubeluka@gmail.com>
2023-03-10 10:11:08 +0100

    Add files via upload

---
+++ helloworld.py
@@ -0,0 +1,87 @@
+a = "Python"
+b = "is"
+c = "awesome!"
+print(a,b,c)
+
+d = 5
+e = 7
+print(d*e)
+
+x = "cool"
+
+def myfunc():
+    x = " wow!"
+    print("Dit is gemaakt door Luka," + x)
+
+myfunc()
+
+print("Dit is heel " + x)
+
+f = 1
+g = 3.6
+h = 8j
+
+print(type(f))
+print(type(g))
+print(type(h))
+
+import random
+print("The Number of the day is" , random.randrange(1, 10))
+
+def myfunc():
+    x = int(1)
+    y = int(2.8)
+    z = int("3")
+    print(x)
+    print(y)
+    print(z)
+
+myfunc()
+
+txt = "The best thing about Python is that is is easy to test code wich is cool."
+if "hard" not in txt:
+    print("No, 'Hard' is NOT present.")
+if "hard" in txt:
+    print("Yes'Hard' is present.")
+
+
+def myfunc():
+    a = "wow,"
+    b = "python."
+    c = a + " " + b
+    print(c)
+myfunc()
+
+quantity = 6
+itemno = 36
+price = 7.99
+myorder  = "I want to pay {2} for {0} pieces of item {1}"
+print(myorder.format(quantity, itemno, price))
+
+def myfunc():
+    a = 1
+    b = 7
+    c = 4
+    d = a+b+c
+    print(d)
+myfunc()
+
+listno1 = ["apple", "banana", "apple", "grape", "Maybe a pear"]
+print(listno1)
+
+list1 = [1, 2, 3, 4, 5]
+list2 = [True, True, False, True, False]
+list3 = ["apple", "samsung", "Xiaomi"]
+list4 = ["1", "3", "4", "6"]
+
+print(list1)
+print(list2[:2])
+print(list3[2])
+
+if "Xiaomi" in list3:
+  print("Yes, 'Xiaomi' is in list3")
+
+list4.insert(1, "2")
+list4.insert(4, "5")
+print(list4)
+
