9d2833f5410dbcef665a3aa2015e302410c24d9b
looka2 <channel.tubeluka@gmail.com>
2023-09-08 19:38:10 +0200

    wiskunde.py

--- wiskunde.py
+++ wiskunde.py
@@ -1,8 +1,22 @@
+import datetime
+
+date = datetime.date(2023, 9, 8)
+
+s = " "
+
+text = "Gemaakt door Luka op de datum"
 
 a = int(input("Wat is het totaal? "))
 
 b = int(input("Wat is de hoeveelheid? "))
 
-c = (360 // a) * (b)
+c = (360 / a) * (b)
+
+print("")
 
 print("de hoek is",c,"graden")
+
+print("")
+
+print(f'{text}{s}{date}')
+
